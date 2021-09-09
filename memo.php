e6b282430f12fce2351e66df382bf5e458cc8b89
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-09-09 15:31:41 +0900

    memo_add

---
+++ memo.php
@@ -0,0 +1,16 @@
+<?php
+$input = trim(fgets(STDIN));
+$inputs = explode(" ", trim(fgets(STDIN)));
+$inputs = array_map('intval', explode(" ", trim(fgets(STDIN))));
+
+
+//読取
+
+
+//計算
+
+
+//結果
+
+
+//関数
