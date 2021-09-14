7e198fcab587201442bc9f150c4b9ae15ec9809c
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-09-14 13:10:27 +0900

    fix

--- 214/214_C.php
+++ 214/214_C.php
@@ -3,7 +3,7 @@ $n = (int) trim(fgets(STDIN));
 $s = array_map('intval', explode(" ", trim(fgets(STDIN))));
 $t = array_map('intval', explode(" ", trim(fgets(STDIN))));
 
-//$n*2 にしているのは、$s+$t の合計要素数が最大で$n*2 のため
+//$n*2 にしているのは、$s+$t の合計要素数が$n*2 のため
 for($i=0; $i<$n*2; $i++){
 	//%$n にしているのは、$i=n*2 の時、インデックスエラーが出てしまうのを回避するため
 	$t[($i+1)%$n] = min($t[($i+1)%$n], $t[$i%$n]+$s[$i%$n]);
