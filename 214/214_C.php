99004db3d33dc70a838311195eaef4b740ab4241
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-09-14 13:03:51 +0900

    add

---
+++ 214/214_C.php
@@ -0,0 +1,12 @@
+<?php
+$n = (int) trim(fgets(STDIN));
+$s = array_map('intval', explode(" ", trim(fgets(STDIN))));
+$t = array_map('intval', explode(" ", trim(fgets(STDIN))));
+
+//$n*2 にしているのは、$s+$t の合計要素数が最大で$n*2 のため
+for($i=0; $i<$n*2; $i++){
+	//%$n にしているのは、$i=n*2 の時、インデックスエラーが出てしまうのを回避するため
+	$t[($i+1)%$n] = min($t[($i+1)%$n], $t[$i%$n]+$s[$i%$n]);
+}
+
+echo implode(PHP_EOL, $t);
