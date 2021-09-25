a7eb4f5665bb4492b2122c9a42490f88ef3520dd
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-09-25 18:50:59 +0900

    implove

--- memo.php
+++ memo.php
@@ -1,17 +1,6 @@
 <?php
 
-
 //関数
-// function int($N=1){
-// 	return fscanf(STDIN, str_repeat("%d", $N));
-// }
-// function str($N=1){
-// 	return fscanf(STDIN, str_repeat("%s", $N));
-// }
-// function float($N=1){
-// 	return fscanf(STDIN, str_repeat("%f", $N));
-// }
-
 function istr():string{
 	return trim(fgets(STDIN));
 }
