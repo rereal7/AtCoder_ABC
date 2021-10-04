44626bcfd2edd8767938cd87cc244037438de3ae
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-10-04 13:21:54 +0900

    fix

--- 221/221_B.py
+++ 221/221_B.py
@@ -1,14 +1,16 @@
-# 未完成のコード
 s = input()
 t = input()
 
 ans = 'No'
 for i in range(len(s)-1):
-	s_1 = s
+	if s == t:
+		ans = 'Yes'
+		break
+	s_1 = list(s)
 	tmp = s_1[i]
 	s_1[i] = s_1[i+1]
 	s_1[i+1] = tmp 
-	if s_1 == t:
+	if ''.join(s_1) == t:
 		ans = 'Yes'
 		break
 
