33a871ae0af1071b902ac51fec0519c840e3e675
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-10-08 10:18:32 +0900

    add

--- 205/205_B.py
+++ 205/205_B.py
@@ -1,8 +1,10 @@
 n = int(input())
 A = list(map(int, input().split()))
-x = n*(n+1)/2
+# x = n*(n+1)/2
 # set()は引数がmapでも使える
-if sum(A) == x and len(set(A)) == n:
+# if sum(A) == x and len(set(A)) == n:
+# リストの中に重複がなければ問題ない
+if len(set(A)) == n:
 	print('Yes')
 else:
 	print('No')
