3c14d33de9c5f95ca89a5d83522cbb2ed4c5bd9d
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-10-08 15:51:57 +0900

    add

---
+++ 205/205_C.py
@@ -0,0 +1,29 @@
+# LTE(1≤C≤10**9 Cは指数)
+a,b,c = map(int,input().split())
+d = pow(a,c)
+e = pow(b,c)
+if d<e:
+	print('<')
+elif d>e:
+	print('>')
+else:
+	print('=')
+
+# 再考
+a,b,c = map(int,input().split())
+if c%2 == 0:
+	a = abs(a)
+	b = abs(b)
+	if a == b:
+		print('=')
+	elif a > b:
+		print('>')
+	elif a < b:
+		print('<')
+else:
+	if a > b:
+		print('>')
+	elif a < b:
+		print('<')
+	else:
+		print('=')
