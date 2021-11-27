9c504d42251bb8cbb8f0f3e08ada71163d10452a
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-11-27 22:31:20 +0900

    AC

--- 228/228_C.py
+++ 228/228_C.py
@@ -1,5 +1,14 @@
 n,k = map(int, input().split())
 P = [list(map(int, input().split())) for _ in range(n)]
 totalScore = [sum(P[i]) for i in range(n)]
+sortedScore = sorted(totalScore, reverse=True)
 
 # print(totalScore)
+# print(sortedScore)
+
+for i in range(n):
+	p = totalScore[i]
+	if p+300 >= sortedScore[k-1]:
+		print('Yes')
+	else:
+		print('No')
