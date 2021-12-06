7f20d1fcb2dcbe17fdf4ea576cb3827c64d597d6
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-12-06 21:14:29 +0900

    refactoring

--- 229/229_C.py
+++ 229/229_C.py
@@ -2,22 +2,31 @@ n,w = map(int, input().split())
 AB = [list(map(int, input().split())) for _ in range(n)]
 AB.sort(reverse=True)
 
+#############
+# リファクタリング後
 ans = 0
-count = 0
 for i in range(n):
-	nice = AB[i][0]
-	g = AB[i][1]
+	ans += AB[i][0] * min(w, AB[i][1])
+	w -= min(w, AB[i][1])
 
-	if count >= w:
-		break
+print(ans)
 
-	if w - count >= g:
-		ans += nice * g
-		count += g
-	else:
-		ans += nice * (w - count)
-		count += (w - count)
+# ans = 0
+# count = 0
+# for i in range(n):
+# 	nice = AB[i][0]
+# 	g = AB[i][1]
+
+# 	if count >= w:
+# 		break
+
+# 	if w - count >= g:
+# 		ans += nice * g
+# 		count += g
+# 	else:
+# 		ans += nice * (w - count)
+# 		count += (w - count)
 
 	# print(f'ans{ans} count{count}')
 
-print(ans)
+# print(ans)
